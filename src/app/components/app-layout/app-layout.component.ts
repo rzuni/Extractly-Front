300fcd0a5fe1bfb563cc4a5b5e318f837a23d1a6
rzuni <151058391+rzuni@users.noreply.github.com>
2025-07-21 12:28:29 -0600

    removing old code

--- src/app/components/app-layout/app-layout.component.ts
+++ src/app/components/app-layout/app-layout.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-//import { TopbarComponent } from './elements/topbar/topbar.component';
-//import { SidebarComponent } from './elements/sidebar/sidebar.component';
 import { CommonModule } from '@angular/common';
 import { LayoutService } from '../../services/layout.service';
 import { SvgIconComponent } from '../svg-icon/svg-icon.component';
@@ -12,8 +10,6 @@ import { SvgIconComponent } from '../svg-icon/svg-icon.component';
   imports: [
     CommonModule,
     RouterOutlet,
-    //TopbarComponent,
-   // SidebarComponent,
     SvgIconComponent
   ],
   templateUrl: './app-layout.component.html',
